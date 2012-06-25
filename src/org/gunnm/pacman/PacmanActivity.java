4a032ef0e775937fc979239e69c0c6740ff9d5f6
Julien <julien@gunnm.org>
2012-06-25 16:07:50 -0400

    Also call the main canvas

--- src/org/gunnm/pacman/PacmanActivity.java
+++ src/org/gunnm/pacman/PacmanActivity.java
@@ -4,10 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class PacmanActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+	
+	GameCanvas mainCanvas;
+	
+	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        mainCanvas = new GameCanvas (this);
+        setContentView(mainCanvas);
+        //setContentView(R.layout.main);
     }
 }
