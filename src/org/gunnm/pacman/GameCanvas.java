74b4375ef5256fd6aa8a990555fcb604050621b7
Julien <julien@gunnm.org>
2012-06-25 17:02:15 -0400

    Start to draw the map

--- src/org/gunnm/pacman/GameCanvas.java
+++ src/org/gunnm/pacman/GameCanvas.java
@@ -4,32 +4,59 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 
 public class GameCanvas extends View
 {
 	private static String FLAG = "GameCanvas";
+	private int size;
 	
 	public GameCanvas (Context context)
 	{
 		super (context);
-		setMinimumHeight(160);
-		setMinimumWidth(105);
+		setMinimumHeight(100);
+		setMinimumWidth(100);
+		WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+		Display display = wm.getDefaultDisplay();
+	
+		if (display.getHeight() < display.getWidth())
+		{
+			size = display.getHeight();
+		}
+		else
+		{
+			size = display.getWidth();
+		}
 	}
 	
 	public void onMeasure (int widthMeasureSpec, int heightMeasureSpec)
 	{
-		setMeasuredDimension(getSuggestedMinimumHeight(), getSuggestedMinimumWidth());
+		
+		setMeasuredDimension(widthMeasureSpec, heightMeasureSpec);
+		
 	}
 	
 	public void draw (Canvas canvas)
 	{
 		Paint color;
-		color = new Paint ();
-		color.setColor(Color.RED);
-		canvas.drawColor(Color.GREEN);
-		canvas.drawText ("Hello", 40, 55, color);
-		canvas.drawLine(50, 50, 100,100,color );
+		int i;
+		int j;
+		int squareSize;
+		
+		squareSize = size / 11;
+		for (i = 0 ; i < 10 ; i++)
+		{
+			for (j = 0 ; j < 10 ; j++)
+			{
+				color = new Paint ();
+				color.setColor(Color.RED);
+				canvas.drawRect(i * squareSize, j * squareSize, (i + 1) * squareSize, (j + 1) * squareSize, color);
+			}
+		}
+		
 		canvas.save();
 	}
 }
