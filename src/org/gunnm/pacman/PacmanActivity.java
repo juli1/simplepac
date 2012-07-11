ece31a31fb506a02c751eceef64ebc07530045e8
Julien <julien@gunnm.org>
2012-07-11 16:51:47 -0400

    Publish update on the market

--- src/org/gunnm/pacman/PacmanActivity.java
+++ src/org/gunnm/pacman/PacmanActivity.java
@@ -16,7 +16,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
